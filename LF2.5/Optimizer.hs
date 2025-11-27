0bb386df75dbaa036789fa0fcfc8ca3f466d612b
Tokarski <thiago.tokarski2001@gmail.com>
2025-11-27 00:18:15 -0300

    add syb

--- LF2.5/Optimizer.hs
+++ LF2.5/Optimizer.hs
@@ -1,101 +1,38 @@
+-- https://gist.github.com/eusuke/3833162
+
 module Optimizer where
 
 import AbsLF
+import Data.Generics
 import Interpreter
 
 optimizeP :: Program -> Program
-optimizeP (Prog fs) = Prog (map optimizeF fs)
+optimizeP = everywhere (mkT optimize)
 
-optimizeF :: Function -> Function
-optimizeF (Fun tR id decls exp) = Fun tR id decls (optimizeE exp)
-
-optimizeE :: Exp -> Exp
-optimizeE exp = case exp of
+optimize :: Exp -> Exp
+optimize exp = case exp of
   EStr str -> EStr str
   ETrue -> ETrue
   EFalse -> EFalse
   EInt n -> EInt n
   EVar id -> EVar id
-  ENot exp ->
-    let optExp = optimizeE exp
-        optENot = ENot optExp
-        (v, ctx) = eval [] optENot
-     in if isLiteral optExp
-          then wrapValueExpression v
-          else optENot
-  ECon exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optECon = ECon optExp0 optExp
-        (v, ctx) = eval [] optECon
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optECon
-  EAdd exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optEAdd = EAdd optExp0 optExp
-        (v, ctx) = eval [] optEAdd
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optEAdd
-  -- TODO: substitua "undefined" abaixo pela otimização correspondente ao tipo de expressão.
-  -- @dica: estude a implementação fornecida da otimização das expressões anteriores
-  ESub exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optESub = ESub optExp0 optExp
-        (v, ctx) = eval [] optESub
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optESub
-  EMul exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optEMul = EMul optExp0 optExp
-        (v, ctx) = eval [] optEMul
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optEMul
-  EDiv exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optEDiv = EDiv optExp0 optExp
-        (v, ctx) = eval [] optEDiv
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optEDiv
-  EOr exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optEOr = EOr optExp0 optExp
-        (v, ctx) = eval [] optEOr
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optEOr
-  EAnd exp0 exp ->
-    let optExp0 = optimizeE exp0
-        optExp = optimizeE exp
-        optEAnd = EOr optExp0 optExp
-        (v, ctx) = eval [] optEAnd
-     in if isLiteral optExp0 && isLiteral optExp
-          then wrapValueExpression v
-          else optEAnd
-  -- TODO: saiba explicar o motivo da otimização abaixo
-  --  ECall id lexp -> ECall id (map (\expr -> optimizeE expr) lexp)
-  ECall id lexp -> ECall id (map optimizeE lexp)
-  -- TODO: crie um programa exemplo em que a otimização abaixo seja usada
-  EIf exp expT expE ->
-    let optExp = optimizeE exp
-        optThen = optimizeE expT
-        optElse = optimizeE expE
-        optEIf = EIf optExp optThen optElse
-     in case optExp of
-          EInt vExpIf ->
-            if vExpIf == 0
-              then optElse
-              else optThen
-          _ -> optEIf
+  ENot e1 -> foldIfConstant exp [e1]
+  ECon exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EAdd exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  ESub exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EMul exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EDiv exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EOr exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EAnd exp0 exp1 -> foldIfConstant exp [exp0, exp1]
+  EIf (EInt v) expT expE -> if v /= 0 then expT else expE
+  _ -> exp
+
+foldIfConstant :: Exp -> [Exp] -> Exp
+foldIfConstant originalExp children
+  | all isLiteral children =
+      let (v, _) = eval [] originalExp
+       in wrapValueExpression v
+  | otherwise = originalExp
 
 isLiteral :: Exp -> Bool
 isLiteral exp = case exp of
